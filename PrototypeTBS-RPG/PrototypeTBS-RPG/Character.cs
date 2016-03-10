a041c9c8bc05319b338baf0dafdeec1ccc782073
amp200123 <a.m.phelps17@gmail.com>
2016-03-10 14:11:45 -0500

    Some basic Day 1 setup

---
+++ PrototypeTBS-RPG/PrototypeTBS-RPG/Character.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrototypeTBS_RPG
+{
+    class Character
+    {
+
+        public Specialization spec { get; private set; }
+        public string name { get; private set; }
+
+        public Character(string name, Specialization spec)
+        {
+            this.spec = spec;
+            this.name = name;
+        }
+    }
+}
---
+++ PrototypeTBS-RPG/PrototypeTBS-RPG/Specialization.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrototypeTBS_RPG
+{
+    abstract class Specialization
+    {
+
+    }
+}
