5ccee28800fdef31e2a500ec99f8fd192ae5bf0b
amp200123 <a.m.phelps17@gmail.com>
2016-04-15 11:35:53 -0400

    Re-added some files

---
+++ PrototypeTBS-RPG/PrototypeTBS-RPG/Items/IronSword.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrototypeTBS_RPG.Items
+{
+    class IronSword : Weapon
+    {
+        public IronSword(ContentManager content)
+            : base(content.Load<Texture2D>("Weapons/IronSword"), "Iron Sword", weaponType.sword, 4, 0, 100, 1, 1)
+        {
+
+        }
+    }
+}
